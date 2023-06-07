de42e1bfd8f5a33677465846be79214efd4bdd16
문지원 <81554184+jeewonMoon@users.noreply.github.com>
2023-06-07 13:51:55 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    int n = Integer.parseInt(br.readLine());
+    String star = new String("*");
+
+    for (int i=1; i<=n; i++) {
+        for (int j=i;j<n;j++) {
+            bw.write(" ");
+        }
+        bw.write(star+"\n");
+        star+="*";
+    }
+    br.close();
+    bw.flush();
+    bw.close();
+    }
+}
